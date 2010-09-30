c75501637c1090a75c6f873f9a57fcd36a3efefc
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2010-09-30 16:22:43 +0200

    use macro names for the exit codes

--- chunker.c
+++ chunker.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define EC_EOF_REACHED 1
+#define EC_LIMIT_REACHED 0
+#define EC_ERROR 2
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "No chunk size given!\n");
-        return 2;
+        return EC_ERROR;
     }
     
     // how big are the chunks?
@@ -12,7 +16,7 @@ int main(int argc, char *argv[]) {
     // how much data is (possibly) left to process?
     long unsigned int left = chunk_size;
     
-    int returncode = 0;
+    int returncode = EC_LIMIT_REACHED;
 
     int buffer_size = 8;
     void *buffer = malloc( buffer_size );
@@ -24,10 +28,10 @@ int main(int argc, char *argv[]) {
         readBytes = read( 0, buffer, toread );
         // fprintf(stderr, "Read %d bytes\n", readBytes);
         if (readBytes == 0) {
-            returncode = 1;
+            returncode = EC_EOF_REACHED;
             break;
         } else if (readBytes < 0) {
-            returncode = 2;
+            returncode = EC_LIMIT_REACHED;
             fprintf(stderr, "Read error\n");
             break;
         } else {
